0ff79b2ebd06e18f5bc7bb63168d5b80d467ffc5
Matt-GitHub <matthewjamesbedard@outlook.com>
2019-12-13 07:16:06 -0500

    display elements to dom from apod api, styled using style components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+// import Image from "./components/images";
+// import ImageTitle from "./components/title";
+// import ImageDescription from "./components/description";
+// import Date from "./components/date";
+// import Copyright from "./components/copyright";
+import ImageCard from "./components/imageCard";
 
 function App() {
+  const [image, setImage] = useState();
+  const [imageTitle, setImageTitle] = useState();
+  const [description, setDescription] = useState();
+  const [date, setDate] = useState();
+  const [copyright, setCopyright] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=D6tKo8m5kkynaYvswFokSgb6Ff3YXgs0xRfzw94s"
+      )
+      .then(response => {
+        setImage(response.data.url);
+        setImageTitle(response.data.title);
+        setDescription(response.data.explanation);
+        setDate(response.data.date);
+        setCopyright(response.data.copyright);
+      })
+      .catch(error => {
+        console.log(error);
+      }, []);
+  });
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
+    <ImageCard
+      title={imageTitle}
+      url={image}
+      cr={copyright}
+      date={date}
+      description={description}
+    />
+    // <div className="App">
+    //   <header>
+    //     <h1>Astronomy Picture of The Day</h1>
+    //   </header>
+    //   <section>
+    //     <div className="imageBox">
+    //       <ImageTitle title={imageTitle} />
+    //       <Image url={image} />
+    //       <Copyright cr={copyright} />
+    //       <Date date={date} />
+    //     </div>
+    //     <ImageDescription description={description} />
+    //   </section>
+    // </div>
   );
 }
 
---
+++ src/components/imageStyle.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  font-size: 1.8rem;
+  text-align: center;
+`;
+
+export const StyledH2 = styled.h2`
+  font-size: 1.7rem;
+  text-align: center;
+`;
+
+export const StyledImg = styled.img`
+  width: 400px;
+  display: flex;
+  margin: 0 auto;
+`;
+
+export const StyledP = styled.p`
+  display: flex;
+  margin: 0 auto;
+  font-size: 0.9rem;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+export const StyledDesP = styled.p`
+    width: 60%
+    display: flex;
+    margin: 0 auto;
+    font-size: 1.1rem;
+    margin-top: 10px;
+    font-weight: 550;
+`;
